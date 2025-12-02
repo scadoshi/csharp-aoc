aca8c99157c8e5ead9f72572389b1ce029486e52
scadoshi <scottyfermo@hotmail.com>
2025-12-02 15:22:01 -0800

    generate input once

--- Program.cs
+++ Program.cs
@@ -1,11 +1,13 @@
-using CSharpAoc.Year2025.Day01;
+using CSharpAoc.Year2025.Day02;
+
+var input = GetInput.Run();
 
 DateTime startedAt = DateTime.Now;
-var result1 = Part01.Run(GetInput.Run());
+var result1 = Part01.Run(input);
 TimeSpan elapsed = DateTime.Now - startedAt;
 Console.WriteLine("part_one={0}, run_time={1}", result1, elapsed);
 
 startedAt = DateTime.Now;
-var result2 = Part02.Run(GetInput.Run());
+var result2 = Part02.Run(input);
 elapsed = DateTime.Now - startedAt;
 Console.WriteLine("part_two={0}, run_time={1}", result2, elapsed);
