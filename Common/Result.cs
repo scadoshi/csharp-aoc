71235c86adeda89559395602b10d87b48cecc718
scadoshi <scottyfermo@hotmail.com>
2026-01-07 14:32:46 -0800

    build Result let's go

---
+++ Common/Result.cs
@@ -0,0 +1,28 @@
+public abstract record Result<TValue, TError>
+{
+    public sealed record Success(TValue Value) : Result<TValue, TError>;
+
+    public sealed record Failure(TError Error) : Result<TValue, TError>;
+
+    public static Success Succ(TValue value)
+    {
+        return new Result<TValue, TError>.Success(value);
+    }
+
+    public static Failure Fail(TError error)
+    {
+        return new Result<TValue, TError>.Failure(error);
+    }
+
+    public TValue GetOrThrow()
+    {
+        return this switch
+        {
+            Success success => success.Value,
+            Failure failure => throw new InvalidOperationException(
+                $"Cannot get success value (TValue) from a Failure. TError: {failure.Error}"
+            ),
+            _ => throw new InvalidOperationException("Unknown result state"),
+        };
+    }
+}
