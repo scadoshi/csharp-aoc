d38384ffeadd0ba2acf518077ed8975b72047e12
scadoshi <scottyfermo@hotmail.com>
2026-01-07 16:07:47 -0800

    Progress

--- Year2016/Day01/Instruction.cs
+++ Year2016/Day01/Instruction.cs
@@ -1,12 +1,15 @@
 namespace CSharpAoc.Year2016.Day01;
+
+using Common;
+
 public class Instruction
 {
-    public readonly Turning.Direction direction;
+    public readonly TurnDirection turn;
     public readonly int distance;
 
-    public Instruction(Turning.Direction direction, int distance)
+    public Instruction(TurnDirection turn, int distance)
     {
-        this.direction = direction;
+        this.turn = turn;
         this.distance = distance;
     }
 
@@ -15,9 +18,11 @@ public class Instruction
         // e.g. "R5"
         char[] chars = value.ToCharArray();
         char directionChar = chars[0];
-        Turning.Direction turningDirection = Turning.Parse(directionChar);
+        var direction = Direction.TryParse(directionChar).GetOrThrow();
+        var turnDirection = TurnDirection.TryParse(direction).GetOrThrow();
         string distanceString = new string(chars[1..]);
         int distance = int.Parse(distanceString);
         return new Instruction(turningDirection, distance);
     }
 }
+
