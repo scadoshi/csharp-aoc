5313238f101099492b908dee8692d6eb6860cc20
scadoshi <scottyfermo@hotmail.com>
2026-01-09 15:56:16 -0800

    Finished up day 1

--- Year2016/Day02/Part01.cs
+++ Year2016/Day02/Part01.cs
@@ -1,9 +1,29 @@
 namespace CSharpAoc.Year2016.Day02;
 
+using CSharpAoc.Common;
+
 public class Part01
 {
-    public static int Run(int input)
+    public static string Run(Face[][] input)
     {
-        return 0;
+        return input
+            .Aggregate(
+                ("", Keypad.Standard()),
+                ((string, Keypad) acc, Face[] instructions) =>
+                {
+                    var (code, keypad) = acc;
+                    var newKeypad = instructions.Aggregate(
+                        keypad,
+                        (k, face) =>
+                        {
+                            k.MoveFinger(face, 1);
+                            return k;
+                        }
+                    );
+                    var key = newKeypad.Grid[newKeypad.Finger.Row, newKeypad.Finger.Col];
+                    return (code + key, newKeypad);
+                }
+            )
+            .Item1;
     }
 }
